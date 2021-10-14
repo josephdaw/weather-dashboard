0f7c7858405ac37a10671c094356416ba22d7768
josephdaw <josephdaw@icloud.com>
2021-10-14 23:38:41 +1030

    add functions for preparing weather, and setting units

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
-
+// element variables
 const fetchButton = $('#fetch-button');
 const forecastContainerEl = $('#forecast-container');
 const longRngForecastContainerEl = $('#long-range-forecast-container');
 
 // variables for user selection of units and city selection
 let unitChoice, cityName;
+
+// apiKey 
+const apiKey = "a1c1d7b47658fe7dae2174e70fccbcd7";
 
 // object list with unit types based on user selection
 const units =
@@ -25,8 +28,6 @@ const units =
 
 // function to get information from the API
 function getAPI() {
-  console.log('getAPI start')
-  const apiKey = "a1c1d7b47658fe7dae2174e70fccbcd7";
   const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${unitChoice}&appid=${apiKey}`;
 
   fetch(queryURL)
@@ -34,42 +35,26 @@ function getAPI() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-      // variables for items
-      const roundTemp = Math.round(data.main.temp);
-      const roundHumidity = Math.round(data.main.humidity);
-      const roundWindSpeed = Math.round(data.wind.speed);
-      const roundUV = Math.round(data.main.temp);
 
-      let temperatureUnit;
-      let windSpeedUnit;
+      // call function to prepare weather data
+      const weather = prepareWeather(data)
 
-      // set unit values
-      if (unitChoice == "metric") {
-        temperatureUnit = units.temperature.metric;
-        windSpeedUnit = units.windSpeed.metric;
-      };
-      if (unitChoice == "standard") {
-        temperatureUnit = units.temperature.standard;
-        windSpeedUnit = units.windSpeed.standard;
-      };
-      if (unitChoice == "imperial") {
-        temperatureUnit = units.temperature.imperial;
-        windSpeedUnit = units.windSpeed.imperial;
-      };
+      // call function to get user selected units
+      const selectedUnit = setUnits();
 
       $('#location-name').text(data.name);
-      $('#temp-now').text(`Temperature: ${roundTemp}\xB0${temperatureUnit}`);
-      $('#humidity-now').text(`Humidity: ${roundHumidity}%`);
-      $('#wind-now').text(`Wind: ${data.wind.deg}\xB0T at ${roundWindSpeed} ${windSpeedUnit}`);
-      $('#uv-now').text(`UV Index: ${roundUV} NEED TO FIX`);
+      $('#temp-now').text(`Temperature: ${weather.temp}\xB0${selectedUnit.temperatureUnit}`);
+      $('#humidity-now').text(`Humidity: ${weather.humidity}%`);
+      $('#wind-now').text(`Wind: ${data.wind.deg}\xB0T at ${weather.windSpeed} ${selectedUnit.windSpeedUnit}`);
+      // $('#uv-now').text(`UV Index: ${roundUV} NEED TO FIX`);
 
       displayForecastEl()
+
     });
 };
 
 function get5Day() {
-  console.log('get5Day start')
+  // console.log('get5Day start')
   const apiKey = "a1c1d7b47658fe7dae2174e70fccbcd7";
   const queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=${unitChoice}&appid=${apiKey}`;
 
@@ -80,33 +65,17 @@ function get5Day() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
-      // console.log(data.list)
 
+      // loop through weather forecast array
       for (i = 0; i < 5; i++) {
-        console.log(data.list[i])
-        // const data = data.list[i];
-        const roundTemp = Math.round(data.list[i].main.temp);
-        const roundHumidity = Math.round(data.list[i].main.humidity);
-        const roundWindSpeed = Math.round(data.list[i].wind.speed);
+        const wxData = data.list[i];
 
-        let temperatureUnit;
-        let windSpeedUnit;
+        // call function to prepare weather data
+        const weather = prepareWeather(wxData)
 
-        // set unit values
-        if (unitChoice == "metric") {
-          temperatureUnit = units.temperature.metric;
-          windSpeedUnit = units.windSpeed.metric;
-        };
-        if (unitChoice == "standard") {
-          temperatureUnit = units.temperature.standard;
-          windSpeedUnit = units.windSpeed.standard;
-        };
-        if (unitChoice == "imperial") {
-          temperatureUnit = units.temperature.imperial;
-          windSpeedUnit = units.windSpeed.imperial;
-        };
-        
+        // call function to get user selected units
+        const selectedUnit = setUnits();
+
         // create parent elements
         const tileEL = $('<div class="tile is-parent">');
         const articleEL = $('<article class="tile is-child box">');
@@ -114,15 +83,15 @@ function get5Day() {
         const unorderListEl = $('<ul>');
 
         // create list elements
-        const tempEl = $('<li>').text(`Temperature: ${roundTemp}\xB0${temperatureUnit}`);
-        const humidityEl = $('<li>').text(`Humidity: ${roundHumidity}%`);
-        const windEl = $('<li>').text(`Wind: ${data.list[i].wind.deg}\xB0T at ${roundWindSpeed} ${windSpeedUnit}`);
-        
+        const tempEl = $('<li>').text(`Temperature: ${weather.temp}\xB0${selectedUnit.temperatureUnit}`);
+        const humidityEl = $('<li>').text(`Humidity: ${weather.humidity}%`);
+        const windEl = $('<li>').text(`Wind: ${data.list[i].wind.deg}\xB0T at ${weather.windSpeed} ${selectedUnit.windSpeedUnit}`);
+
         // append list items
         unorderListEl.append(tempEl);
         unorderListEl.append(humidityEl);
         unorderListEl.append(windEl);
-        
+
         // add elements to the page
         articleEL.append(headingEl);
         articleEL.append(unorderListEl)
@@ -133,6 +102,44 @@ function get5Day() {
     });
 }; //END - get5Day()
 
+
+// create a function to return an object of weather based on user units
+function prepareWeather(data) {
+  // define weather object
+  const weather = {
+    temp: Math.round(data.main.temp),
+    humidity: Math.round(data.main.humidity),
+    windSpeed: Math.round(data.wind.speed),
+  };
+
+  return weather;
+
+};
+
+
+function setUnits() {
+  // set unit values
+  if (unitChoice == "metric") {
+    return {
+      temperatureUnit: units.temperature.metric,
+      windSpeedUnit: units.windSpeed.metric
+    }
+
+  };
+  if (unitChoice == "standard") {
+    return {
+      temperatureUnit: units.temperature.standard,
+      windSpeedUnit: units.windSpeed.standard
+    }
+  };
+  if (unitChoice == "imperial") {
+    return {
+      temperatureUnit: units.temperature.imperial,
+      windSpeedUnit: units.windSpeed.imperial
+    }
+  };
+
+};
 
 // function to unhide forecast container elements
 function displayForecastEl() {
